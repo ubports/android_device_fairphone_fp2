2497cafa5a6409cc4648acddb95fcf55e003a887
wangxl <wangxl@thundersoft.com>
2015-02-04 13:48:38 +0800

    FP2-15: create Product ID for FP2 project
    Change-Id: I14b883d22b5e828356627e3ae412aaeaa91c41e7

---
+++ BoardConfig.mk
@@ -0,0 +1,108 @@
+# config.mk
+#
+# Product-specific compile-time definitions.
+#
+
+ifeq ($(TARGET_ARCH),)
+TARGET_ARCH := arm
+endif
+TARGET_KERNEL_ARCH := arm
+BOARD_USES_GENERIC_AUDIO := true
+USE_CAMERA_STUB := false
+
+TARGET_USES_AOSP := false
+# Compile with msm kernel
+TARGET_COMPILE_WITH_MSM_KERNEL := true
+TARGET_HAS_QC_KERNEL_SOURCE := true
+
+-include $(QCPATH)/common/msm8974/BoardConfigVendor.mk
+
+#TODO: Fix-me: Setting TARGET_HAVE_HDMI_OUT to false
+# to get rid of compilation error.
+TARGET_HAVE_HDMI_OUT := false
+TARGET_USES_OVERLAY := true
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+TARGET_NO_BOOTLOADER := false
+TARGET_NO_KERNEL := false
+TARGET_NO_RADIOIMAGE := true
+TARGET_NO_RPC := true
+
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_CPU_ABI  := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := krait
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+TARGET_HARDWARE_3D := false
+TARGET_BOARD_PLATFORM := msm8974
+TARGET_BOOTLOADER_BOARD_NAME := MSM8974
+
+BOARD_KERNEL_BASE        := 0x00000000
+BOARD_KERNEL_PAGESIZE    := 2048
+BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
+BOARD_RAMDISK_OFFSET     := 0x02000000
+
+# Enables Adreno RS driver
+OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+MAX_EGL_CACHE_SIZE := 2048*1024
+
+# Use signed boot and recovery image
+TARGET_BOOTIMG_SIGNED := true
+
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_PERSISTIMAGE_FILE_SYSTEM_TYPE := ext4
+
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3b7 ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
+BOARD_KERNEL_SEPARATED_DT := true
+
+BOARD_EGL_CFG := device/qcom/$(TARGET_BOARD_PLATFORM)/egl.cfg
+
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01000000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01000000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 10737401856
+BOARD_CACHEIMAGE_PARTITION_SIZE := 33554432
+BOARD_PERSISTIMAGE_PARTITION_SIZE := 5242880
+BOARD_TOMBSTONESIMAGE_PARTITION_SIZE := 73400320
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+
+
+# Add NON-HLOS files for ota upgrade
+ADD_RADIO_FILES ?= true
+
+# Added to indicate that protobuf-c is supported in this build
+PROTOBUF_SUPPORTED := true
+
+TARGET_USE_KRAIT_PLD_SET := true
+TARGET_KRAIT_BIONIC_PLDOFFS := 10
+TARGET_KRAIT_BIONIC_PLDTHRESH := 10
+TARGET_KRAIT_BIONIC_BBTHRESH := 64
+TARGET_KRAIT_BIONIC_PLDSIZE := 64
+
+TARGET_USES_ION := true
+TARGET_USES_NEW_ION_API :=true
+
+TARGET_HW_DISK_ENCRYPTION := false
+
+# Workaround framework bluetooth dependency
+BOARD_HAVE_BLUETOOTH := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/generic/common/bluetooth
+OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+
+TARGET_INIT_VENDOR_LIB := libinit_msm
+TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_msm
+
+TARGET_LDPRELOAD := libNimsWrap.so
