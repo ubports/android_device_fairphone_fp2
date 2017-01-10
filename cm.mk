bfcf6ab14c4841a234871e4bb493a5d546e0e8ca
Marius Gripsgard <mariogrip@ubuntu.com>
2017-01-11 00:43:07 +0100

    Revert "[DO NOT ACCEPT] test code review"
    This reverts commit 7d3fd7b9df00677c532d0567197cd40a42580570.
    
    Change-Id: Ia2d3c54e368f07f44ffc2ff9ea7052f9aa02d7b6

--- cm.mk
+++ cm.mk
@@ -37,7 +37,7 @@ TARGET_VENDOR_PRODUCT_NAME := FP2
 TARGET_VENDOR_DEVICE_NAME := FP2
 PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=FP2 PRODUCT_NAME=FP2
 
-# Use the latest approved GMS identifiers unless running a signed build
+## Use the latest approved GMS identifiers unless running a signed build
 ifneq ($(SIGN_BUILD),true)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="f2p-user 5.1.1 LMY48B YOG4PAS1N0 release-keys"
