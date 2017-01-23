ae6e05294ff3541b0fa18d51a79b49da29af12f3
Ricardo Mendoza <mendoza.ricardo@gmail.com>
2017-01-23 20:03:07 +0100

    Enable wcnss service from binary Fairphone Open blob and adjust startup scripts to match real sysfs structure
    Change-Id: Ia709fc21001a2479b34e752cc1c19dcf7c2465bb

--- rootdir/root/init_wlan.sh
+++ rootdir/root/init_wlan.sh
@@ -1,28 +1,13 @@
 #!/system/bin/sh
 
-# The Ubports project
+## The Ubports project
+## Wait for WCNSS service to setup WLAN device over QMI
 
-# Workaround for conn_init not copying the updated firmware
-rm /data/misc/wifi/WCNSS_qcom_cfg.ini
-rm /data/misc/wifi/WCNSS_qcom_wlan_nv.bin
-
-/system/bin/conn_init
-
-echo 1 > /dev/wcnss_wlan
-
-for i in 1 2 3; do
-    # sleep first to avoid issue when called after conn_init
-    sleep 1
-    if [ ! -f /sys/devices/platform/wcnss_wlan.0/net/wlan0/address ]; then
+while true; do
+    sleep 2
+    if [ ! -f /sys/devices/fb000000.qcom,wcnss-wlan/net/wlan0/address ]; then
         echo sta > /sys/module/wlan/parameters/fwpath
     else
         break
     fi
 done
-
-if [ ! -f /sys/devices/platform/wcnss_wlan.0/net/wlan0/address ]; then
-    # Driver is in a broken state, give it a few seconds and try to reload
-    echo 1 > /dev/wcnss_wlan
-    sleep 2
-    echo sta > /sys/module/wlan/parameters/fwpath
-fi
