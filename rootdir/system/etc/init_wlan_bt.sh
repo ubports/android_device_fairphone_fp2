d37504d14bc74f09e402e8ee445b0658b08dc430
smoose-nils <nils@smoose.nl>
2017-02-10 14:41:41 +0100

    init: add init_wlan.bt.sh
    Change-Id: I3ff2c222aa58b6d6a1619b2c8af4ae76e4e7eddd

---
+++ rootdir/system/etc/init_wlan_bt.sh
@@ -0,0 +1,28 @@
+#!/system/bin/sh
+
+## The Ubports project
+## Wait for WCNSS service to setup WLAN device over QMI
+
+enable_bt () {
+
+        if [[ `getprop ro.qualcomm.bt.hci_transport` != "smd" ]]; then
+            setprop ro.qualcomm.bt.hci_transport smd
+        fi
+
+        #initialize bt device
+        /system/bin/hci_qcomm_init -e 
+        echo 1 > /sys/module/hci_smd/parameters/hcismd_set
+
+
+}
+
+while true; do
+    sleep 2
+    if [ ! -f /sys/devices/fb000000.qcom,wcnss-wlan/net/wlan0/address ]; then
+        echo sta > /sys/module/wlan/parameters/fwpath
+    else
+        # enable bluetooth here since we have to wait for wlan to be initialized
+        enable_bt
+        break
+    fi
+done
